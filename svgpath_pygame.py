963937dbfcb258ddb64d0e20c5130845519b3d39
Revixa1 <57970411+Revixa1@users.noreply.github.com>
2023-06-16 10:21:48 -0400

    great

--- svgpath_pygame.py
+++ svgpath_pygame.py
@@ -1,9 +1,8 @@
 from __future__ import division  # we need floating division
-from svg.path import Path, Line, Arc, CubicBezier, QuadraticBezier, parse_path
 import pygame
 import sys
 from xml.dom import minidom
-from svg.path import Path, Line, Arc, CubicBezier, QuadraticBezier, parse_path
+from svg.path import Path, Move, Line, Arc, CubicBezier, QuadraticBezier, Close, parse_path
 import argparse
 
 
@@ -25,37 +24,43 @@ parserargs.add_argument('-o','--outputFile')
 args=parserargs.parse_args()
 
 print(args)
-print(args.resolution_courbe)
+print(int(args.resolution_courbe))
 print("\n\n\n") # 
 
 # svg.path point method returns a complex number p, p.real and p.imag can pull the x, and y
 # # on 0.0 to 1.0 along path, represent percent of distance along path
-n = args.resolution_courbe  # number of line segments to draw
+n = int(args.resolution_courbe)  # number of line segments to draw
 #nomOutput=
 
 scaling=5
 x0=0
 y0=950
 
-file1 = open(args.inputFile, "w")
+file1 = open(args.outputFile, "w")
 file1.write("")
 file1.close()
 
-mydoc = minidom.parse("./map.svg")
+mydoc = minidom.parse(args.inputFile)
 print(mydoc)
 pygame.init()                                  # init pygame
 surface = pygame.display.set_mode((1000,1000)) # get surface to draw on
 surface.fill(pygame.Color('white'))            # set background to white
 
 path_tag = mydoc.getElementsByTagName("path")
+print(path_tag)
+
+
 
 for j in range(len(path_tag)):
-    
+
+    if path_tag[j].hasAttribute('nom')==False:
+            print("\n",path_tag[j])
+            continue
     
 
     style_string = path_tag[j].attributes['style'].value
 
- 
+    print("on continue la for")
 # getting index of substrings
     idx1 = style_string.find("stroke:")
     idx2 = idx1+14
@@ -67,19 +72,20 @@ for j in range(len(path_tag)):
     string=path_tag[j].attributes['nom'].value
     
     Path_elements = parse_path(d_string)
-    print_pts = [ (('{'+str(p.real))+','+(str(-1*p.imag)+'}')) for p in (Path_elements.point(i/n) for i in range(0, n+1))]
-    for a in range(len(print_pts)):
-        if a == 0:
-            string= string + '{' 
-        string = string + print_pts[a]
-        if a<len(print_pts)-1:
-            string = string + ','
-        
-    string = string + '};\n'
+
+   
     pts = [ ((p.real/scaling)+x0,(p.imag/scaling)+y0) for p in (Path_elements.point(i/n) for i in range(0, n+1))]
+        
+    print_pts = [ (str(p.real)+';'+str(-1*p.imag)) for p in (Path_elements.point(i/n) for i in range(0, n+1))]
+        
+    string = string + '\n' + "Xp ; Yp"
+
+    for i in range(0,n+1):
+        string= string + '\n' + str(print_pts[i])# + ' ' + str(print_pts[i]) 
+    
     pygame.draw.aalines( surface,pygame.Color(color), False, pts) # False is no closing
     print(string)
-    file1 = open(outputFile, "a")
+    file1 = open(args.outputFile, "a")
     file1.write(string)
     file1.close()
 
