8a98ed91343ad86b1d731696de402aa3cd9964b7
Revixa1 <57970411+Revixa1@users.noreply.github.com>
2023-06-15 20:57:20 -0400

    ok

--- svgpath_pygame.py
+++ svgpath_pygame.py
@@ -4,29 +4,45 @@ import pygame
 import sys
 from xml.dom import minidom
 from svg.path import Path, Line, Arc, CubicBezier, QuadraticBezier, parse_path
+import argparse
 
 
 # Si il n'y a pas d'arguments à l'appel du programme, montrer comment utiliser
-if (len(sys.argv) == 1):
-    print('Utilisation:', sys.argv[0],
-          'nombre_de_points_par_courbes ')
-    exit(1)
+#if (len(sys.argv) == 1):
+#    print('Utilisation:', sys.argv[0],
+#          'nombre_de_points_par_courbes ','\n -i input svg file','\n -o output txt file')
+#    exit(1)
 
+parserargs = argparse.ArgumentParser(
+                    prog='ProgramName',
+                    description='What the program does',
+                    epilog='Text at the bottom of help')
+
+parserargs.add_argument('resolution_courbe')
+parserargs.add_argument('-i','--inputFile')
+parserargs.add_argument('-o','--outputFile')
+
+args=parserargs.parse_args()
+
+print(args)
+print(args.resolution_courbe)
 print("\n\n\n") # 
 
 # svg.path point method returns a complex number p, p.real and p.imag can pull the x, and y
 # # on 0.0 to 1.0 along path, represent percent of distance along path
-n = int(sys.argv[1])  # number of line segments to draw
+n = args.resolution_courbe  # number of line segments to draw
+#nomOutput=
+
 scaling=5
 x0=0
 y0=950
 
-file1 = open("points.txt", "w")
+file1 = open(args.inputFile, "w")
 file1.write("")
 file1.close()
 
 mydoc = minidom.parse("./map.svg")
-
+print(mydoc)
 pygame.init()                                  # init pygame
 surface = pygame.display.set_mode((1000,1000)) # get surface to draw on
 surface.fill(pygame.Color('white'))            # set background to white
@@ -48,7 +64,7 @@ for j in range(len(path_tag)):
     color = style_string[idx1 + len("stroke:"): idx2]
     
     d_string = path_tag[j].attributes['d'].value
-    string=path_tag[j].attributes['nom'].value+"[""][2]="
+    string=path_tag[j].attributes['nom'].value
     
     Path_elements = parse_path(d_string)
     print_pts = [ (('{'+str(p.real))+','+(str(-1*p.imag)+'}')) for p in (Path_elements.point(i/n) for i in range(0, n+1))]
@@ -63,7 +79,7 @@ for j in range(len(path_tag)):
     pts = [ ((p.real/scaling)+x0,(p.imag/scaling)+y0) for p in (Path_elements.point(i/n) for i in range(0, n+1))]
     pygame.draw.aalines( surface,pygame.Color(color), False, pts) # False is no closing
     print(string)
-    file1 = open("points.txt", "a")
+    file1 = open(outputFile, "a")
     file1.write(string)
     file1.close()
 
