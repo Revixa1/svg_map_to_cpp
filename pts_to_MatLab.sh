8b3bca153c0b6412d7177f12dbb206d9a4e083cf
Revixa1 <57970411+Revixa1@users.noreply.github.com>
2023-06-20 13:03:19 -0400

    lol

--- pts_to_MatLab.sh
+++ pts_to_MatLab.sh
@@ -1,1 +1,1 @@
-python3 svgpath_pygame.py 200 -i trajet.svg -o "/home/revixa1/txgui/OneDrive - USherbrooke/COURS/S3/projet/Controle/Code_simulink/points.txt"
+python3 svgpath_pygame.py 10000 -i trajet.svg -o "/home/revixa1/txgui/OneDrive - USherbrooke/COURS/S3/projet/Controle/Code_simulink/points.txt"
